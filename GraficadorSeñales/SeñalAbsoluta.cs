72850d5340d6787b6de1b8dff0ca9399ae18a7ad
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-09-03 10:41:18 -0700

    Agregar archivos de proyecto.

---
+++ GraficadorSeñales/SeñalAbsoluta.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraficadorSeñales
+{
+    class SeñalAbsoluta
+    {
+        public List<Muestra> Muestras
+        { get; set; }
+
+        public SeñalAbsoluta()
+        {
+            Muestras =
+                new List<Muestra>();
+        }
+
+        public double evaluar(double tiempo)
+        {
+            double resultado;
+
+            resultado = Math.Abs(tiempo);
+
+            return resultado;
+        }
+    }
+}
